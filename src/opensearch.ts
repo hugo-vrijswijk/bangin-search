f20a08e908383460ac0078caafc913c9331e0a33
Hugo van Rijswijk <git@hugovr.nl>
2026-05-27 10:31:34 +0200

    Fix opensearch host behind reverse proxies

--- src/opensearch.ts
+++ src/opensearch.ts
@@ -6,17 +6,17 @@ export interface OpenSearchParams {
   suggestions: string;
 }
 
-export function makeOpenSearchXml(params: OpenSearchParams): string {
+export function makeOpenSearchXml({ shortName, description, longName, baseUrl, suggestions }: OpenSearchParams): string {
   return `<OpenSearchDescription
   xmlns="http://a9.com/-/spec/opensearch/1.1/"
   xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-  <ShortName>${params.shortName}</ShortName>
-  <Description>${params.description}</Description>
-  <LongName>${params.longName}</LongName>
+  <ShortName>${shortName}</ShortName>
+  <Description>${description}</Description>
+  <LongName>${longName}</LongName>
   <InputEncoding>UTF-8</InputEncoding>
-  <Image width="16" height="16" type="image/x-icon">${new URL('/favicon.ico', params.baseUrl)}</Image>
-  <Url type="text/html" template="${new URL('/search?q={searchTerms}', params.baseUrl)}"/>
-  <Url type="application/x-suggestions+json" method="GET" template="${params.suggestions}"/>
-  <Url type="application/opensearchdescription+xml" rel="self" template="${new URL('/opensearch.xml', params.baseUrl)}" />
+  <Image width="16" height="16" type="image/x-icon">${new URL('/favicon.ico', baseUrl)}</Image>
+  <Url type="text/html" template="${new URL('/search?q={searchTerms}', baseUrl)}"/>
+  <Url type="application/x-suggestions+json" method="GET" template="${suggestions}"/>
+  <Url type="application/opensearchdescription+xml" rel="self" template="${new URL('/opensearch.xml', baseUrl)}" />
 </OpenSearchDescription>`;
 }
--- src/pages/opensearch.xml.ts
+++ src/pages/opensearch.xml.ts
@@ -1,10 +1,14 @@
 import type { APIRoute } from 'astro';
 import { makeOpenSearchXml } from '../opensearch';
 
-export const GET: APIRoute = ({ url }) => {
+export const GET: APIRoute = ({ url, request }) => {
+  const proto = request.headers.get('x-forwarded-proto') ?? url.protocol;
+  const origin = request.headers.get('x-forwarded-host') ?? request.headers.get('host') ?? url.host;
+  const baseUrl = `${proto}://${origin}`;
+
   return new Response(
     makeOpenSearchXml({
-      baseUrl: url.origin,
+      baseUrl,
       suggestions: 'https://api.qwant.com/v3/suggest/?q={searchTerms}&amp;client=opensearch',
       description: 'Search the web with fast Bangs',
       longName: "Bangin' Search",
