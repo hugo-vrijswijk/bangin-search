34ada5ea27c042d1beb63808e73940dfeee26750
Hugo van Rijswijk <git@hugovr.nl>
2026-05-27 15:15:14 +0200

    Update

--- src/pages/search.ts
+++ src/pages/search.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from 'astro';
 import { getBangRedirectUrl } from '../bang';
 import { DEFAULT_BANG } from 'astro:env/server';
 
-export const GET: APIRoute = ({ url }) => {
+export const GET: APIRoute = ({ url, redirect }) => {
   const searchUrl = getBangRedirectUrl(url, DEFAULT_BANG) ?? '/';
-  return Response.redirect(searchUrl, 301);
+  return redirect(searchUrl, 301);
 };
